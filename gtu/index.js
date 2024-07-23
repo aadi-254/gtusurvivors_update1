c2ac3d9437646b96e4f1de4a05925f0a4f9ad21e
aadi-254 <161030423+aadi-254@users.noreply.github.com>
2024-07-23 21:07:10 +0530

    Add files via upload

---
+++ gtu/index.js
@@ -0,0 +1,137 @@
+
+    gsap.registerPlugin(ScrollTrigger);
+
+
+            // Page 2 Animations
+            gsap.from(".head", {
+                y: 100,
+                opacity: 0,
+                duration: 1.3,
+                scrollTrigger: {
+                    trigger: ".head",
+                    start: "top 100%",
+                    toggleActions: "restart none none none",
+                    markers: false
+                }
+            });
+
+            gsap.from(".he1", {
+                y: 100,
+                opacity: 0,
+                duration: 1.3,
+                scrollTrigger: {
+                    trigger: ".he1",
+                    start: "top 100%",
+                    toggleActions: "restart none none none",
+                    markers: false
+                }
+            });
+
+            gsap.from(".syllabus", {
+                x: -260,
+                opacity: 0,
+                duration: 1.3,
+                scrollTrigger: {
+                    trigger: ".syllabus",
+                    start: "top 100%",
+                    toggleActions: "restart none none none",
+                    markers: false
+                }
+            });
+
+            gsap.from(".image3", {
+                x: 260,
+                opacity: 0,
+                duration: 1.3,
+                scrollTrigger: {
+                    trigger: ".image3",
+                    start: "top 100%",
+                    toggleActions: "restart none none none",
+                    markers: false
+                }
+            });
+
+            gsap.from(".materials", {
+                x: -260,
+                opacity: 0,
+                duration: 1.3,
+                scrollTrigger: {
+                    trigger: ".materials",
+                    start: "top 100%",
+                    toggleActions: "restart none none none",
+                    markers: false
+                }
+            });
+
+            gsap.from(".image4", {
+                x: 260,
+                opacity: 0,
+                duration: 1.3,
+                scrollTrigger: {
+                    trigger: ".image4",
+                    start: "top 100%",
+                    toggleActions: "restart none none none",
+                    markers: false
+                }
+            });
+
+            gsap.from(".pyqs", {
+                x: -260,
+                opacity: 0,
+                duration: 1.3,
+                scrollTrigger: {
+                    trigger: ".pyqs",
+                    start: "top 100%",
+                    toggleActions: "restart none none none",
+                    markers: false
+                }
+            });
+
+            gsap.from(".image5", {
+                x: 260,
+                opacity: 0,
+                duration: 1.3,
+                scrollTrigger: {
+                    trigger: ".image5",
+                    start: "top 100%",
+                    toggleActions: "restart none none none",
+                    markers: false
+                }
+            });
+
+            gsap.from(".pcgpa", {
+                x: -200,
+                opacity: 0,
+                duration: 1.3,
+                scrollTrigger: {
+                    trigger: ".pcgpa",
+                    start: "top 100%",
+                    toggleActions: "restart none none none",
+                    markers: false
+                }
+            });
+
+            gsap.from(".fhead", {
+                y: -200,
+                opacity: 0,
+                duration: 1.3,
+                scrollTrigger: {
+                    trigger: ".fhead",
+                    start: "top 100%",
+                    toggleActions: "restart none none none",
+                    markers: false
+                }
+            });
+
+            gsap.from(".hlogo", {
+                x: -200,
+                opacity: 0,
+                duration: 1.3,
+                scrollTrigger: {
+                    trigger: ".hlogo",
+                    start: "top 100%",
+                    toggleActions: "restart none none none",
+                    markers: false
+                }
+            });
+
